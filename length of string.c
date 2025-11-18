24ef5b3875dc1673353f8323a35e93a897a2a326
Eng.Kerubo <faithkerush06@gmail.com>
2025-11-18 17:40:07 +0300

    Add files via upload

---
+++ length of string.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+char str[20];
+printf("kerubo");
+int length=strlen(str);
+printf("The length of string is:%d\n",length);
+scanf("%19s\n",str);
+printf("%s\n",str);
+
+return 0;}
